05800ba6610bf3663a780cd92bbebee0daf16bc4
zhangjihao <zhangjihao@panda.tv>
2017-08-01 13:54:36 +0800

    同步master

--- src/flame/net/net.cpp
+++ src/flame/net/net.cpp
@@ -30,31 +30,6 @@ namespace net {
 		class_tcp_server.add<&tcp_server::close>("close");
 		class_tcp_server.add<&tcp_server::__destruct>("__destruct");
 		ext.add(std::move(class_tcp_server));
-		
-		// class_client
-		// ------------------------------------
-		if(curl_global_init(CURL_GLOBAL_ALL)) {
-			php::exception("curl_glbal_init fail", -1);
-		}
-
-		ext.add<http::get>("flame\\net\\http\\get");
-		ext.add<http::post>("flame\\net\\http\\post");
-		ext.add<http::put>("flame\\net\\http\\put");
-
-		php::class_entry<http::request> class_request("flame\\net\\http\\request");
-		class_request.add(php::property_entry("url", ""));
-		class_request.add(php::property_entry("method", ""));
-		class_request.add(php::property_entry("timeout", 10));
-		class_request.add(php::property_entry("header", nullptr));
-		class_request.add(php::property_entry("body", nullptr));
-		class_request.add<&http::request::__construct>("__construct");
-		class_request.add<&http::request::__destruct>("__destruct");
-		ext.add(std::move(class_request));
-
-		php::class_entry<http::client> class_client("flame\\net\\http\\client");
-		class_client.add<&http::client::exec>("exec");
-		class_client.add<&http::client::debug>("debug");
-		ext.add(std::move(class_client));
 	}
 	php::string addr2str(const struct sockaddr_storage& addr) {
 		php::string str(64);
