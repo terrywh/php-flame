1d9af3f5367e1cd3669bb7c297f4ac7d8e7dc71e
terrywh <terry.wuhao@gmail.com>
2017-08-24 18:42:38 +0800

    补充注释

--- test/flame/net/fastcgi/server.php
+++ test/flame/net/fastcgi/server.php
@@ -1,10 +1,12 @@
 <?php
+// 可选，初始化框架配置
 flame\init("fastcgi-server", [
-	"worker" => 2, // 启动两个工作进程
+	"worker" => 2, // 启动两个工作进程，默认 0
 ]);
 flame\go(function() {
+	// 创建 fastcgi 服务器
 	$server = new flame\net\fastcgi\server();
-
+	// 设置处理程序
 	$server->handle("/hello", function($req, $res) {
 		var_dump($req->method, $req->body); // GET POST PUT ....
 		yield $res->write("hello ");
@@ -14,6 +16,7 @@ flame\go(function() {
 		yield $res->write_header(404);
 		yield $res->end();
 	})
+	// 默认处理程序（即：不匹配上述路径形式时调用）
 	->handle(function($req, $res) {
 		echo "----- ", getmypid(), " -----\n";
 		$data = json_encode($req);
